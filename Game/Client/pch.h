a4fa2838f00c5271152ab5d974be674849fc7412
Hyeonu-HomePC <renewal1990@gamil.com>
2021-11-02 13:09:42 +0900

    Data Upload

---
+++ Game/Client/pch.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#pragma comment(lib, "Engine.lib") //속성에서 링크에서 추가하던거 이렇게 코드로
+
+#include "EnginePch.h"
+
+//Engine.lib에 있어서 이제 필요없다
+//#include <vector>
+//#include <memory>
+//using namespace std;
+
