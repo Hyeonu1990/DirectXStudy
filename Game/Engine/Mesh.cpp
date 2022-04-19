195e44b16e0e202f3210643017eb87ad97e63780
HyeounChoi-IntelMac <renewal1990@gmail.com>
2022-04-19 17:08:14 +0900

    ConstantBuffer

--- Game/Engine/Mesh.cpp
+++ Game/Engine/Mesh.cpp
@@ -38,8 +38,12 @@ void Mesh::Render()
 	CMD_LIST->IASetVertexBuffers(0, 1, &_vertexBufferView); // Slot: (0~15)
 
 	// TODO
-	// 1) Buffer에다가 데이터 세팅
+	// 1) Buffer에다가 데이터 세팅 - 위 Init에 CreateCommittedResource 관련 부분
 	// 2) Buffer의 주소를 register에다가 전송
+	// 커맨드리스트에 적립되고 수행됨으로 즉시 발동되는게 아님
+	// 그에 따른 커맨드리스트 수행 중 버퍼값 변경에 주의해야함
+	GEngine->GetCB()->PushData(0, &_transform, sizeof(_transform));
+	GEngine->GetCB()->PushData(1, &_transform, sizeof(_transform));
 	
 	CMD_LIST->DrawInstanced(_vertexCount, 1, 0, 0);
 }
